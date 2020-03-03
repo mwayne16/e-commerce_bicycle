cc28e869e523326033ca8379ca4411e48d028ad0
Marcus Davis <mwayn316@gmail.com>
2020-03-03 13:04:37 -0600

    changed to iterable

--- src/layout/Shop.js
+++ src/layout/Shop.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Shop.css';
+
+function Products() {
+  const [products, updateProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        'https://raw.githubusercontent.com/mwayne16/e-commerce_bicycle/master/src/application.json'
+      )
+      .then(res => {
+        updateProducts(...res.data);
+      });
+  }, []);
+  return (
+    <ul>
+      <li>{products.name}</li>
+    </ul>
+  );
+}
+
 function Shop() {
   return (
     <section className="product-collection">
       <h1 className="collection-header">Our Collection</h1>
+      <Products />
     </section>
   );
 }
